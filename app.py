ef4e1983691171d80aee6b1afde97c513a626223
KingTheSim <KingTheSim@abv.bg>
2023-05-27 23:59:50 +0300

    base

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, redirect
+from body.blockchain import Blockchain
+
+# Web app creation
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "test_key"
+
+# Creates object of the class blockchain.
+blockchain = Blockchain()
+
+
+# Mining a new block
+@app.route("/api/v1/blocks/mine", methods=["POST"])
+def mine_block():
+    previous_block = blockchain.print_previous_block()
+    previous_proof = previous_block["proof"]
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    # Block building
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {"message": "A block is mined",
+                "index": block["index"],
+                "timestamp": block["timestamp"],
+                "proof": block["proof"],
+                "previous_hash": block["previous_hash"],
+                }
+
+    return jsonify(response), 200
+
+
+# Displaying blockchain
+@app.route("/api/v1/blocks", methods=["GET"])
+def display_chain():
+    response = {"chain": blockchain.chain,
+                "len": len(blockchain.chain)}
+    return jsonify(response), 200
+
+
+# Validity checker
+@app.route("/api/v1/blocks/validity", methods=["GET"])
+def valid():
+    validity = blockchain.chain_valid(blockchain.chain)
+
+    if validity:
+        response = {"message": "The blockchain is valid."}
+    else:
+        response = {"message": "The blockchain is invalid"}
+    return jsonify(response), 200
+
+
+# Default route. Redirects to the blockchain
+@app.route("/", methods=["GET"])
+def index():
+    return redirect("/api/v1/blocks")
+
+
+# Run flask server locally
+app.run(host="127.0.0.1", port=5000)
---
+++ body/blockchain.py
@@ -0,0 +1,92 @@
+import datetime
+import hashlib
+import json
+import os.path
+
+
+class Blockchain:
+    # First block creation function
+    def __init__(self):
+        self.chain = []
+        self.chain_loader()
+        if not self.chain:
+            self.create_block(proof=1, previous_hash="0")
+
+    # Block generator
+    def create_block(self, proof, previous_hash):
+        block = {"index": len(self.chain) + 1,
+                 "timestamp": str(datetime.datetime.now()),
+                 "proof": proof,
+                 "previous_hash": previous_hash,
+                 }
+        self.chain.append(block)
+        self.block_saver(block)
+        return block
+
+    def block_saver(self, block):
+        filename = "blockchain.json"
+        if not os.path.isfile(filename):
+            with open(filename, "w") as f:
+                json.dump([block], f)
+
+        else:
+            with open(filename, "r") as f:
+                chain = json.load(f)
+            chain.append(block)
+            with open(filename, "w") as f:
+                json.dump(chain, f)
+
+    def _file_reader(self, filename):
+        with open(filename, "r") as f:
+            content = f.read().strip()
+        return content
+
+    def chain_loader(self):
+        filename = "blockchain.json"
+        if not os.path.isfile(filename):
+            return
+
+        content = self._file_reader(filename)
+        if not content:
+            return
+        blocks = f"[{content}]"
+        self.chain = json.loads(blocks)
+
+    # Previous block viewer
+    def print_previous_block(self):
+        return self.chain[-1]
+
+    # Proof-of-work miner
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        fail_limit = 100000
+
+        while not check_proof and new_proof < fail_limit:
+            hash_operation = hashlib.sha256(
+                f"{(new_proof ** 2 - previous_proof ** 2)}".encode()).hexdigest()
+            if hash_operation[:5] == "00000":
+                check_proof = True
+            else:
+                new_proof += 1
+
+        return new_proof if check_proof else None
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def chain_valid(self, chain):
+        if len(self.chain) < 2:
+            return True
+
+        last, second_last = self.chain[-1], self.chain[-2]
+        if last["previous_hash"] != self.hash(second_last):
+            return False
+
+        previous_proof = second_last["proof"]
+        proof = last["proof"]
+
+        hashing = hashlib.sha256(f"{proof ** 2 - previous_proof ** 2}".encode()).hexdigest()
+
+        return True
